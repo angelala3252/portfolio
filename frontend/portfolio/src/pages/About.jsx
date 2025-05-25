70c987308d44e0f2d31460df94e7a46c71d9daa1
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2025-05-25 02:37:10 -0400

    about me

--- frontend/portfolio/src/pages/About.jsx
+++ frontend/portfolio/src/pages/About.jsx
@@ -2,7 +2,24 @@ import '../style.css';
 
 const About = () => {
     return (
-        <></>
+        <>
+            <div className='page-content'>
+                <div className='paragraph'>
+                    <p>
+                        I'm a software developer based in Toronto, Ontario.
+                    </p>
+                    <p>
+                        Currently, I'm a product engineering intern at Ontario Teachers' Pension Plan.
+                    </p>
+                    <p>
+                        I've been into photography since high school, and recently I've been playing around with a new film camera.
+                    </p>
+                    <p>
+                        I enjoy gaming, especially puzzle and strategy games like Tetris, Minesweeper, Balatro, and Slay the Spire. I'm also always reading something — whether that be a book, manga, or webtoon. My goal is to read 25 books for 2025.
+                    </p>
+                </div>
+            </div>
+        </>
     )
 };
 
