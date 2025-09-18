b5445c220caeb1fac7757ba530e462c9ab520431
Angela Zhuo <angelazhuo@hotmail.com>
2025-09-17 22:00:57 -0400

    photo gallery

--- frontend/portfolio/src/pages/Photography.jsx
+++ frontend/portfolio/src/pages/Photography.jsx
@@ -1,19 +1,33 @@
-import { useEffect, useState } from "react";
-import Gallery from "react-photo-gallery";
+const photoFilenames = [
+    "cny1.JPG",
+    "cny2.JPG",
+    "cny3.JPG",
+    "cny4.JPG",
+    "cny5.JPG",
+    "cny6.JPG",
+    "cny7.JPG",
+    "cny8.JPG",
+    "cny9.JPG",
+    "cny10.JPG",
+    "cny11.JPG",
+    "cny12.JPG",
+    "cny13.JPG",
+    "pride1.JPG",
+    "pride2.JPG",
+    "pride3.JPG"
+];
 
 const Photography = () => {
-    const [photos, setPhotos] = useState([]);
-
-    useEffect(() => {
-        fetch("/photos.json")
-            .then((res) => res.json())
-            .then(setPhotos);
-    }, []);
-
     return (
         <div className="page-content">
             <h1>Corporate/Event Photography</h1>
-            <Gallery photos={photos} />
+            <div className="photo-grid">
+                {photoFilenames.map((filename, idx) => (
+                    <div key={idx} className="photo-item">
+                        <img src={require(`./images/photography/${filename}`)} alt={`photo-${idx}`} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
