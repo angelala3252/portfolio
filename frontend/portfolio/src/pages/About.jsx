3980ccb683f8e8ca209b4eb71ca7aef59b5425e9
Angela Zhuo <angelazhuo@hotmail.com>
2025-09-17 23:27:10 -0400

    add images to icon and about page

--- frontend/portfolio/src/pages/About.jsx
+++ frontend/portfolio/src/pages/About.jsx
@@ -4,19 +4,22 @@ const About = () => {
     return (
         <>
             <div className='page-content'>
-                <div className='paragraph'>
-                    <p>
-                        I'm a software developer based in Toronto, Ontario.
-                    </p>
-                    <p>
-                        Currently, I'm a product engineering intern at Ontario Teachers' Pension Plan.
-                    </p>
-                    <p>
-                        I've been into photography since high school, and recently I've been playing around with a new film camera.
-                    </p>
-                    <p>
-                        I enjoy gaming, especially puzzle and strategy games like Tetris, Minesweeper, Balatro, and Slay the Spire. I'm also always reading something — whether that be a book, manga, or webtoon. My goal is to read 25 books for 2025.
-                    </p>
+                <div className='about'>
+                    <img src={require('./images/me.jpg')} alt="Me" />
+                    <div className='paragraph'>
+                        <p>
+                            I'm a software developer based in Toronto, Ontario.
+                        </p>
+                        <p>
+                            Currently, I'm a product engineering intern at Ontario Teachers' Pension Plan.
+                        </p>
+                        <p>
+                            I've been into photography since high school, and recently I've been playing around with a new film camera.
+                        </p>
+                        <p>
+                            I enjoy gaming, especially puzzle and strategy games like Tetris, Minesweeper, Balatro, and Slay the Spire. I'm also always reading something — whether that be a book, manga, or webtoon. My goal is to read 25 books for 2025.
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
