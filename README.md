909801b3d8c2dad7864654ddec01e16c0c0c092d
Angela Zhuo <angelazhuo@hotmail.com>
2025-09-17 23:08:30 -0400

    add instagram embed

--- README.md
+++ README.md
@@ -9,14 +9,14 @@ npm run start
 
 # TODO
     - is there a way to embed ig profile with all the pics
-- about page
-make home page look better
 wip sign for coding projects
 put pic of riot for the icon
-- design
 - responsive
 - cursor styling
+deploy
 - add to resume, ig, apply to jobs!
 //
+- design
+make home page look better
 - filters? way to show diff projects
 - coding projects page
--- frontend/portfolio/src/pages/Photography.jsx
+++ frontend/portfolio/src/pages/Photography.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const photoFilenames = [
     "cny1.JPG",
     "cny2.JPG",
@@ -19,16 +21,38 @@ const photoFilenames = [
 ];
 
 const Photography = () => {
+    const [category, setCategory] = useState("corporate");
+
     return (
         <div className="page-content">
-            <h1>Corporate/Event Photography</h1>
-            <div className="photo-grid">
-                {photoFilenames.map((filename, idx) => (
-                    <div key={idx} className="photo-item">
-                        <img src={require(`./images/photography/${filename}`)} alt={`photo-${idx}`} />
-                    </div>
-                ))}
+            <div className="photography-menu">
+                <button
+                    onClick={() => setCategory("corporate")}
+                    className={category === "corporate" ? "active" : ""}
+                >
+                    Corporate/Events
+                </button>
+                <button
+                    onClick={() => setCategory("street")}
+                    className={category === "street" ? "active" : ""}
+                >
+                    Street/Portrait
+                </button>
             </div>
+
+            {category === "corporate" ?
+                <div className="photo-grid">
+                    {photoFilenames.map((filename, idx) => (
+                        <div key={idx} className="photo-item">
+                            <img src={require(`./images/photography/${filename}`)} alt={`photo-${idx}`} />
+                        </div>
+                    ))}
+                </div>
+                : <div className="instagram-widget">
+                    <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+                    <iframe src="//lightwidget.com/widgets/1f6c17526a58553c98293b0df713c6c0.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style={{"width": "100%", "border": "0", "overflow": "hidden"}}></iframe>
+                </div>
+            }
         </div>
     );
 };
